6d9531d74a3ba2fa6f44c82d48d71f2cf7ad0c34
luiz.almeida <luiz.almeida@trixti.com.br>
2023-08-15 12:12:53 -0300

    forms

---
+++ src/screens/Authenticated/CreateAd/forms/productsForm/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Form = styled.View`
+  margin: 55px 0px;
+
+  width: 100%;
+`
+
+export const ContentModal = styled.View`
+    flex: 1;
+    padding: 24px;
+
+`
+
+export const Errors = styled.Text`
+  font-size: 13px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.main};
+
+  margin-bottom: 5px;
+  margin-left: 2px;
+`
+
+export const Footer = styled.View`
+  width: 100%;
+  margin-top: 40px;
+`
